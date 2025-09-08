9df00cfb6a719926913a5b587aa83eabef6125bf
Divyanshu Sharma <divyanshu9304628@gmail.com>
2025-09-08 18:57:05 +0530

    Update Footer.jsx

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -1,7 +1,7 @@
 "use client";
 
 import React from "react";
-import Link from "next/link";
+import Link from "next/link"; 
 import { Twitter, Github, Mail, Linkedin } from "lucide-react";
 
 const Footer = () => {
